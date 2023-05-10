3bb62eafb9ab5dbf69b98cf1c270483ad79bdccd
= <=>
2023-05-10 15:02:53 +0700

    create vm with static ip

---
+++ Terraform/build-with-public-static-ip.tf
@@ -0,0 +1,21 @@
+resource "google_compute_address" "vm_static_ip" {
+  name = "terraform-static-ip"
+  address_type = "EXTERNAL"  # menentukan tipe alamat IP publik
+}
+ 
+resource "google_compute_instance" "vm_instance" {
+  name         = "terraform-instance"
+  machine_type = "f1-micro"
+  tags         = ["web", "dev"]
+  boot_disk {
+    initialize_params {
+      image = "debian-cloud/debian-11"
+    }
+  }
+  network_interface {
+    network = google_compute_network.vpc_network.self_link
+    access_config {
+      nat_ip = google_compute_address.vm_static_ip.address
+    }
+  }
+}
